ca12bda3cbcbb783bbe602e29b99dbc9b7116737
Devesh Rawat <devsrawt@gmail.com>
2019-01-19 15:38:37 +0530

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,12 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
 var config = {
-  apiKey: "AIzaSyD_9RaQCgqQAQp3UFRFEoN-Ub8maYkG7wk",
-  authDomain: "projectplanner-35319.firebaseapp.com",
-  databaseURL: "https://projectplanner-35319.firebaseio.com",
-  projectId: "projectplanner-35319",
-  storageBucket: "projectplanner-35319.appspot.com",
-  messagingSenderId: "702684864473"
+ 
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
