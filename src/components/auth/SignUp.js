a772dda3c6b28de232676332e02925b245598e3d
Devesh <devsrawt@gmail.com>
2019-01-20 13:43:57 +0530

    firebse functions setup

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -32,15 +32,30 @@ class SignUp extends Component {
           </div>
           <div className="input-field">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange} />
+            <input
+              type="password"
+              id="password"
+              onChange={this.handleChange}
+              required
+            />
           </div>
           <div className="input-field">
             <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" onChange={this.handleChange} />
+            <input
+              type="text"
+              id="firstName"
+              onChange={this.handleChange}
+              required
+            />
           </div>
           <div className="input-field">
             <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" onChange={this.handleChange} />
+            <input
+              type="text"
+              id="lastName"
+              onChange={this.handleChange}
+              required
+            />
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
