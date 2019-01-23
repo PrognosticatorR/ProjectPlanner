abf707a37d2a0219d1b4a39dcf5a73b5f3ff9a5b
Devesh <devsrawt@gmail.com>
2019-01-23 11:51:54 +0530

    commit after some style changes

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,26 +1,40 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { signOut } from '../../store/actions/authActions'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
 
-const SignedInLinks = (props) => {
+const SignedInLinks = props => {
   return (
     <div>
       <ul className="right">
-        <li><NavLink to='/create'>New Project</NavLink></li>
-        <li><a onClick={props.signOut}>Log Out</a></li>
-        <li><NavLink to='/' className="btn btn-floating pink lighten-1">
-          {props.profile.initials}
-        </NavLink></li>
+        <li>
+          <NavLink to="/create">
+            Add Project &nbsp; <i className="fas fa-calendar-plus fa-1x" />
+          </NavLink>
+        </li>
+        <li>
+          <a onClick={props.signOut}>
+            LogOut &nbsp;
+            <i className="fas fa-sign-out-alt fa-1x" />
+          </a>
+        </li>
+        <li>
+          <NavLink to="/" className="btn btn-floating pink lighten-1">
+            {props.profile.initials}
+          </NavLink>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     signOut: () => dispatch(signOut())
-  }
-}
+  };
+};
 
-export default connect(null, mapDispatchToProps)(SignedInLinks)
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignedInLinks);
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const SignedOutLinks = () => {
   return (
     <div>
       <ul className="right">
-        <li><NavLink to='/signup'>Signup</NavLink></li>
-        <li><NavLink to='/signin'>Login</NavLink></li>
+        <li>
+          <NavLink to="/signup">
+            Signup &nbsp;
+            <i className="fas fa-user-plus fa-1x " />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/signin">
+            Login &nbsp;
+            <i className=" fas fa-sign-in-alt fa-1x" />
+          </NavLink>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SignedOutLinks
+export default SignedOutLinks;
